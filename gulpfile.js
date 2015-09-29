810ef0f5b167dd3020f805ca2f17d1bcc4ee5ec7
Nick Salloum <nick@callmenick.com>
2015-09-29 14:06:32 -0700

    (refactor) refactors build tool and css stuffs

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,98 @@
-// require gulp
-var gulp = require('gulp');
+(function() {
 
-// require plugins
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var gutil = require('gulp-util');
-var minifyCSS = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sass = require('gulp-ruby-sass');
-var uglify = require('gulp-uglify');
+  'use strict'
 
-// js task
-gulp.task('js', function() {
-  return gulp.src('./js/src/*.js')
-    .pipe(concat('demo.js'))
-    .pipe(gulp.dest('./js/dist/'))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js/dist/'));
-});
+  /**
+   * Requirements
+   */
+  var gulp = require('gulp');
+  var autoprefixer = require('gulp-autoprefixer');
+  var cssmin = require('gulp-cssmin');
+  var concat = require('gulp-concat');
+  var jshint = require('gulp-jshint');
+  var notify = require("gulp-notify");
+  var rename = require('gulp-rename');
+  var sass = require('gulp-sass');
+  var stylish = require('jshint-stylish');
+  var uglify = require('gulp-uglify');
 
-// styles task
-gulp.task('styles', function() {
-  return sass('./sass', {
-      style: 'expanded',
-      noCache: true
-    })
-    .on('error', function(err) {
-      gutil.beep();
-      console.error(err);
-      this.emit('end');
-    })
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./css/'))
-    .pipe(minifyCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css/'));
-});
+  /**
+   * Paths
+   */
+  var paths = {
+    sass: ['./sass/**/*.scss'],
+    scripts: ['./js/src/**/*.js']
+  };
 
-// default task
-gulp.task('default', ['js', 'styles', 'watch']);
+  /**
+   * Styles
+   */
+  gulp.task('styles', function() {
+    return gulp.src(paths.sass)
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }))
+      .on('error', notify.onError({
+        title: 'Error compiling Sass',
+        message: 'Check the console for info'
+      }))
+      .on('error', sass.logError)
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./css'))
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css'));
+  });
 
-// watcher
-gulp.task('watch', function() {
-  gulp.watch('./src/js/*.js', ['js']);
-  gulp.watch('./sass/*.scss', ['styles']);
-});
+  /**
+   * Styles watcher
+   */
+  gulp.task('stylesDev', function() {
+    gulp.watch(paths.sass, ['styles']);
+  });
+
+  /**
+   * Scripts linting
+   */
+  gulp.task('lint', function() {
+    return gulp.src(paths.scripts)
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish))
+      .pipe(jshint.reporter('fail'))
+      .on('error', notify.onError({
+        title: 'JS hint failed',
+        message: 'Check the console for errors'
+      }));
+  });
+
+  /**
+   * Scripts concat
+   */
+  gulp.task('concat', function() {
+    return gulp.src(paths.scripts)
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('./js/dist/'))
+      .pipe(uglify())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('./js/dist/'));
+  });
+
+  /**
+   * Scripts dev
+   */
+  gulp.task('scriptsDev', function() {
+    gulp.watch(paths.scripts, ['lint']);
+  });
+
+  /**
+   * Final tasks - these are the tasks that should be run from the command line,
+   * as they encompass the above.
+   */
+  gulp.task('default', ['styles', 'lint', 'stylesDev', 'scriptsDev']);
+  gulp.task('styles:dev', ['styles', 'stylesDev']);
+  gulp.task('scripts:dev', ['lint', 'scriptsDev']);
+  gulp.task('scripts:build', ['lint', 'concat']);
+
+})();
